339c50995e670854b8c0b6444670164c4e29157c
Logan Watanabe <loganwatanabe@gmail.com>
2014-10-29 00:38:46 -0400

    chnaged name of trash to solutions

--- webpage/javascript/form.js
+++ webpage/javascript/form.js
@@ -6,7 +6,51 @@ $(function() { //document is loaded and ready
 		callback(taken_names);
 	}
 
+	$('#submit-button').click(function(){
+		var name = $('#name').val();
+		var email = $('#email').val();
+		var username = $('#username').val();
+		var gender = $('input[name=gender]:checked', '#page-form').val();
+
+		if(name, username, email, gender == null){
+			alert('please fill out required fields');
+			return false;
+		}else{
+			return true;
+		}
+		return false;
+	})
 	
+
+	$('#username').focusout(function(){
+		name_check(function(taken_names){
+
+			var username = $('#username').val();
+			if(username == ''){
+
+				$('#username').css({'background-color':'#FF8080'});
+				$('#username').parent().append('<br> username is required!');
+				$('#username').next().next().remove();
+				
+
+				return false
+
+			}else if ($.inArray(username, taken_names) >=0){
+
+				$('#username').css({'background-color':'#FF8080'});
+				$('#username').parent().append('<br> username is taken!');
+								$('#username').next().next().remove();
+				return false
+
+			}else{
+				$('#username').css({'background-color':'#ADEBAD'});
+				$('#username').parent().append('<br> username is yours!');
+								$('#username').next().next().remove();
+				return true
+			}
+
+		})
+	});
 
 
 });//onReady
--- webpage/javascript/index.js
+++ webpage/javascript/index.js
@@ -1,7 +1,28 @@
-(function() {	//the document is loaded, and this function is carried out
+(function(){	//the document is loaded, and this function is carried out
 
+alert("hI");
 
+var image_array = ['images/index1.jpeg', 'images/index2.jpg', 'images/index3.jpg', 'images/index4.jpg'];
+var index = 2;
+window.setInterval(function(){
+	document.getElementById("index-main-pic").src=image_array[index%4];
+	index +=1;
+	console.log("image changed");
+}, 5000);
 
+window.onload = function(){
+	document.getElementById("color-button").onclick = function(){
+		var hex_char=[0,1,2,3,4,5,6,7,8,9, "A", "B", "C", "D", "E", "F"];
+		var random = "#";
+
+		do{
+			random +=hex_char[Math.floor(Math.random() * hex_char.length)]
+		}while(random.length<7)
+
+		document.getElementById('color-changer').setAttribute("style", 'background-color: '+random+';');
+		console.log(random);
+	};
+};
 
 
 })();
